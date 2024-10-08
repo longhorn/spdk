85dc1f3a4a9eca1d97ea19a1983c5c89bfc317d9
Damiano Cipriani <damiano.cipriani@suse.com>
2024-10-08 16:02:44 +0200

    nvme: nvme_ctrlr_clear_changed_ns_log async process
    The function nvme_ctrlr_clear_changed_ns_log is now executed
    asynchronously in the nvme controller processing of async events.
    This commit is the first step about handling namespace attribute
    change event asynchronously.
    
    Change-Id: I5d92afb3b32dc4451953c612539e603aad8b5340
    Signed-off-by: Damiano Cipriani <damiano.cipriani@suse.com>

--- lib/nvme/nvme_internal.h
+++ lib/nvme/nvme_internal.h
@@ -877,7 +877,21 @@ enum nvme_ctrlr_state {
 #define NVME_TIMEOUT_KEEP_EXISTING	UINT64_MAX
 
 struct spdk_nvme_ctrlr_aer_completion {
+	struct spdk_nvme_ctrlr	*ctrlr;
 	struct spdk_nvme_cpl	cpl;
+
+	/*
+	 * Pointer to the payload buffer for the get log page command to be
+	 * executed in the async handling of this completion event.
+	 */
+	char			*buffer;
+
+	/*
+	 * Counter to be used in the update of active namespaces to keep track
+	 * of how many ns construct executions have been started.
+	 */
+	uint32_t		ns_count;
+
 	STAILQ_ENTRY(spdk_nvme_ctrlr_aer_completion) link;
 };
 
