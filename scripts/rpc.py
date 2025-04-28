44d9a263764b82eb12f42bfd63072b2e500443ba
Damiano Cipriani <damiano.cipriani@suse.com>
2025-04-28 11:50:10 +0200

    vbdev_lvol: add ranged shallow copy RPC
    Add RPC interface to start a ranged shallow copy of a read only
    lvol over a given bdev.
    
    Longhorn 10037
    
    Signed-off-by: Damiano Cipriani <damiano.cipriani@suse.com>

--- scripts/rpc.py
+++ scripts/rpc.py
@@ -2237,6 +2237,20 @@ Format: 'user:u1 secret:s1 muser:mu1 msecret:ms1,user:u2 secret:s2 muser:mu2 mse
     p.add_argument('dst_bdev_name', help='destination bdev name')
     p.set_defaults(func=bdev_lvol_start_shallow_copy)
 
+    def bdev_lvol_start_range_shallow_copy(args):
+        print_json(rpc.lvol.bdev_lvol_start_range_shallow_copy(args.client,
+                                                               src_lvol_name=args.src_lvol_name,
+                                                               dst_bdev_name=args.dst_bdev_name,
+                                                               clusters=args.clusters))
+
+    p = subparsers.add_parser('bdev_lvol_start_range_shallow_copy',
+                              help="""Start a range shallow copy of an lvol over a given bdev.  The
+    status of the operation can be obtained with bdev_lvol_check_shallow_copy""")
+    p.add_argument('src_lvol_name', help='source lvol name')
+    p.add_argument('dst_bdev_name', help='destination bdev name')
+    p.add_argument('--clusters', help="""List of clusters indexes. Example: 2 3 4 etc""", type=int , nargs='+')
+    p.set_defaults(func=bdev_lvol_start_range_shallow_copy)
+
     def bdev_lvol_check_shallow_copy(args):
         print_json(rpc.lvol.bdev_lvol_check_shallow_copy(args.client,
                                                          operation_id=args.operation_id))
